cd0ffb9b4c70cbf860fe880f5c3aaa6156fe524c
Juan Carlos <113791085+Juank2003@users.noreply.github.com>
2023-09-17 19:52:39 +0000

    CodigoComoEnClase

---
+++ Lanzador.java
@@ -0,0 +1,42 @@
+// El lanzador controlara el flujo de la aplicación, lanzando un proceso Procesador Contabilidad para cada archivo de entrada y luego agregando los resultados en un archivo de salida.
+// Autor: Diego Revuelta (diegorevuelta.com)
+// Fecha: 2016-06-05
+// Licencia: GPL v3
+
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+
+public class Lanzador {
+    public static void main(String[] args) {
+        String[] archivos = { "informatica.txt", "gerencia.txt", "contabilidad.txt", "comercio.txt", "recursos_humanos.txt" };
+
+        // Crear hilos para procesar cada archivo
+        Thread[] threads = new Thread[archivos.length];
+        for (int i = 0; i < archivos.length; i++) {
+            threads[i] = new Thread(new ProcesadorContabilidad(archivos[i]));
+            threads[i].start();
+        }
+
+        // Esperar a que todos los hilos terminen
+        for (Thread thread : threads) {
+            try {
+                thread.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+        // Sumar todas las sumas de departamentos
+        long sumaGlobal = UtilidadesFicheros.obtenerSumaTransacciones(archivos);
+
+        // Guardar resultado global
+        try (PrintWriter pw = new PrintWriter(new FileWriter("Resultado_global.txt"))) {
+            pw.println(sumaGlobal);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ Main.java
@@ -0,0 +1,13 @@
+// importar librerias necesarias
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+// clase principal
+
+public class Main{
+    public static void main (String[] args){
+        Lanzador.main(args);
+    }
+
+}
